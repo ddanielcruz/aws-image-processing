5e118ace6e2cccbc0f25c5c0d89e959bd2c07b6b
Daniel Cruz <danielcunha54@gmail.com>
2022-12-15 21:43:20 -0300

    chore: set up Jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+import type { Config } from 'jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.paths.json'
+
+export default {
+  clearMocks: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
+} as Config
--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,5 +1,4 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway'
-import { formatJSONResponse } from '@libs/api-gateway'
+import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway'
 import { middyfy } from '@libs/lambda'
 
 import schema from './schema'
